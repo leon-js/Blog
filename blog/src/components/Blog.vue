0e5dc5bb9ee6cc868fa9deccb9c409ae9f8c7b41
lianglihao <807478609@qq.com>
2018-10-04 15:03:54 +0800

    nodejs链接数据库测试

--- blog/src/components/Blog.vue
+++ blog/src/components/Blog.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="">
-    <button type="primary" @click="getArticle">调用后台接口</button>
     <input v-model="inpContent">
+    <button type="primary" @click="getValue">获取数据</button>
+    <button type="primary" @click="setValue">添加数据</button>
   </div>
 </template>
 
@@ -10,16 +11,26 @@ export default {
   name: 'blog',
   data() {
     return {
-      inpContent: 'Blog'
+      inpContent: ''
     }
   },
   methods: {
-    getArticle() {
-      this.http.get('/api/getArticle')
-        .then( (res) => {
-          console.log('res', res);
-          this.inpContent = res.data.data;
-        })
+    getValue() {
+      // axios.get('/', {params: ''})
+      this.http.get('/api/getValue', {
+        params: {id: 1}
+      }).then( (res) => {
+        console.log('res', res.data);
+        this.inpContent = res.data[0].name;
+      })
+    },
+    setValue() {
+      // axios.post('/', {})
+      this.http.post('/api/setValue', {
+        id: 1, name: this.inpContent
+      }).then( (res) => {
+        console.log('res', res);
+      })
     }
   }
 }
